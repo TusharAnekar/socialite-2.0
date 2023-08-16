4ff39d22dcf5eaf57572b81dac03ec526b28cd46
Tushar Anekar <anekar.tushar@gmail.com>
2023-08-16 15:57:11 +0530

    feat- initial server code added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { makeServer } from './server'
+
+// Call make Server
+makeServer()
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
