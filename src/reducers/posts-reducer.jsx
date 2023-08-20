1b31279c66c3bac3c45fa81bf95173a1fc7239fd
Tushar Anekar <anekar.tushar@gmail.com>
2023-08-20 23:46:09 +0530

    feat- set sort type case

--- src/reducers/posts-reducer.jsx
+++ src/reducers/posts-reducer.jsx
@@ -1,12 +1,14 @@
 export const initialPostsState = {
   allPosts: [],
   allLikedPosts: [],
+  sortType: "Latest"
 };
 
 export const postsReducer = (state, { type, payload }) => {
   switch (type) {
     case "SET_ALL_POSTS":
       return { ...state, allPosts: payload };
+    case "SET_SORT_TYPE": return { ...state, sortType: payload };
     default:
       return state;
   }
