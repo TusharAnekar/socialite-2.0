57e08325b7685177866208f96d6a003fd4f5e0ec
Tushar Anekar <anekar.tushar@gmail.com>
2023-08-20 23:45:28 +0530

    feat- bookmark, like and follow user

--- src/reducers/users-reducer.jsx
+++ src/reducers/users-reducer.jsx
@@ -1,16 +1,31 @@
 export const initialUsersState = {
-    allUsers: [],
-    inputSearch: "",
-    userBookmarks: []
-}
+  allUsers: [],
+  inputSearch: "",
+  userBookmarks: [],
+};
 
-export const usersReducer = (state, {type, payload}) => {
-    switch(type) {
-        case "GET_ALL_USERS": return {...state, allUsers: payload}
-        case "INPUT_SEARCH": return {...state, inputSearch: payload}
-        case "SET_USER_BOOKMARKS": return {...state, userBookmarks: payload}
-        case "ADD_POST_TO_USER_BOOKMARKS": return {...state, userBookmarks: payload}
-        case "REMOVE_POST_FROM_USER_BOOKMARKS": return {...state, userBookmarks: payload}
-        default: return state
-    }
-}
+export const usersReducer = (state, { type, payload }) => {
+  switch (type) {
+    case "GET_ALL_USERS":
+      return { ...state, allUsers: payload };
+    case "INPUT_SEARCH":
+      return { ...state, inputSearch: payload };
+    case "SET_USER_BOOKMARKS":
+      return { ...state, userBookmarks: payload };
+    case "ADD_POST_TO_USER_BOOKMARKS":
+      return { ...state, userBookmarks: payload };
+    case "REMOVE_POST_FROM_USER_BOOKMARKS":
+      return { ...state, userBookmarks: payload };
+    case "ADD_FOLLOWING_USER":
+      return {
+        ...state,
+        allUsers: state.allUsers?.map((user) =>
+          user?._id === payload?._id
+            ? Object.assign(user, { following: payload?.following })
+            : user
+        ),
+      };
+    default:
+      return state;
+  }
+};
