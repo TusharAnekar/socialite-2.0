a4edfc9e1c60d26bfbd2b2b4f3b2fadad909587f
Tushar Anekar <anekar.tushar@gmail.com>
2023-08-22 11:32:36 +0530

    feat- unfllow and edit user

--- src/reducers/users-reducer.jsx
+++ src/reducers/users-reducer.jsx
@@ -2,6 +2,7 @@ export const initialUsersState = {
   allUsers: [],
   inputSearch: "",
   userBookmarks: [],
+  isEditUserModal: false
 };
 
 export const usersReducer = (state, { type, payload }) => {
@@ -16,7 +17,7 @@ export const usersReducer = (state, { type, payload }) => {
       return { ...state, userBookmarks: payload };
     case "REMOVE_POST_FROM_USER_BOOKMARKS":
       return { ...state, userBookmarks: payload };
-    case "ADD_FOLLOWING_USER":
+    case "UPDATE_FOLLOWING_USER":
       return {
         ...state,
         allUsers: state.allUsers?.map((user) =>
@@ -25,6 +26,17 @@ export const usersReducer = (state, { type, payload }) => {
             : user
         ),
       };
+      case "UPDATE_FOLLOWERS_USER":
+        return {
+          ...state,
+          allUsers: state.allUsers?.map((user) =>
+            user?._id === payload?._id
+              ? Object.assign(user, { followers: payload?.followers })
+              : user
+          ),
+        };
+      case "SET_IS_EDIT_USER_MODAL": return{...state, isEditUserModal: payload}
+      case "SET_EDITED_USER": return{ ...state, allUsers: state?.allUsers?.map((user) => user?._id === payload?._id ? Object.assign(user, payload) : user)}
     default:
       return state;
   }
